012f80ee8396117046d81f3898cf000072d03037
Bwalya <gabrielmaele1@yahoo.com>
2024-02-09 18:01:45 -0600

    frontend refactoring, error with internal service
    Ensure that the filename and its extension are correct and match the import statement. Take to troubleshoot and fix the issue.

--- frontend/src/components/Services/ServiceList.jsx
+++ frontend/src/components/Services/ServiceList.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-import { service } from "./../../assets/data/service";
+import { services } from "../../assets/data/services";
 import ServiceCard from "./ServiceCard";
 
 const ServiceList = () => {
     return (
         <div className="grit grid-cols-1 md:grid-cols-2 lg:grid-col-3 gap-5 lg:gap-[30px] mt-[30px] 
         lg:mt-[55px]">
-            {service.map((item,index)=> ( <ServiceCard item={item} index={index} key={index}/>))}
+            {services.map((item,index)=> ( <ServiceCard item={item} index={index} key={index}/>))}
         </div>
     )
 }
---
+++ frontend/src/pages/Cleaners/Cleaner.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import cleanerImg01 from "../assets/images/cleaner01.png";
+
+export const Cleaner = [
+    {
+        id: "01",
+        name: "Maria Rangel",
+        specialization: "Cleaning Professional",
+        avgRating: 4.8,
+        totalRating: 272,
+        photo: cleanerImg01,
+        totalClients: 1500,
+        location: "Frisco 75035/75034, Prosper, Collin County 75078 Celina, Allen 75002/75013, Dallas 75225/75205, The Colony 75056, Plano 75093/75025/75024/75094/75023, Richardson Palisades Central, Carrollton 75010/75007/75006, Garland Hills Of Firewheel Greens Greens Estates Firewheel Estates Hills of Firewheel, Addison 75230/75201 UPTOWN/75254, 75182, Sunnyvale, Dallas County, DeSoto Northwest Estates",
+      },
+]
+export default Cleaner; 
